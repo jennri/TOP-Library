21a30d573bfbfdc605734fcbab79fb105f0d9db1
jennri <121411741+jennri@users.noreply.github.com>
2023-05-24 00:12:20 +1000

    retrieve books added to modal

--- javascript.js
+++ javascript.js
@@ -1,14 +1,38 @@
 
 
+let myLibrary = [];
+
+function Book(title, author, pages, rating){
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.rating = rating;
+
+}
 
 
 
 
+function addBookToLibrary(){
+    let title = document.querySelector("#title").value;
+    let author = document.querySelector("#author").value;
+    let pages = document.querySelector("#pages").value;
+    let rating = document.querySelector("#rating").value;
+        let newBook = new Book(title, author, pages, rating);
+        myLibrary.push(newBook);
+        console.log(myLibrary)
 
 
+}
 
-
-
+//Rather then the ID of the submit btn, you want to refer to the ID of the form
+//No backened so preventDefault() stops data going there
+document.querySelector("#newBookForm").addEventListener("submit", function(event){
+    event.preventDefault();
+    addBookToLibrary();
+    closeModal();
+    })
+    
 
 
 
@@ -21,12 +45,12 @@
 //
 //
 //
-const openModal = () => {
+function openModal () {
     addModal.style.display = 'block';
     modalContainer.style.display = 'block';
 }
 
-const closeModal = () => {
+function closeModal () {
     addModal.style.display = 'none';
     modalContainer.style.display = 'none';
 }
