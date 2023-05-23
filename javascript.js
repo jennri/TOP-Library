196408e59a2cd439b2c065fbdcee9ddd046b1df5
jennri <121411741+jennri@users.noreply.github.com>
2023-05-23 16:22:16 +1000

    Skeleton HTML/CSS
    HTML and CSS foundations built. Display toggle for the search modal work.

---
+++ javascript.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+//Add scale for rating
+//Add library 
+
+
+//
+//
+//
+//Opening and closing functions for the add book btn
+//
+//
+//
+const openModal = () => {
+    addModal.style.display = 'block';
+    modalContainer.style.display = 'block';
+}
+
+const closeModal = () => {
+    addModal.style.display = 'none';
+    modalContainer.style.display = 'none';
+}
+
+const addBtn = document.querySelector("#addbutton");
+addBtn.addEventListener('click', openModal)
+
+const modalContainer = document.querySelector('#addModalContainer')
+modalContainer.addEventListener('click', closeModal)
